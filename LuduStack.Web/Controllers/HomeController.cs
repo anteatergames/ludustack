e8bfda1a3f21e839ffb0fb170d8105e57f4458b9
Daniel Gomes <programad@gmail.com>
2020-06-28 16:53:16 -0300

    Description and about page changed.

--- LuduStack.Web/Controllers/HomeController.cs
+++ LuduStack.Web/Controllers/HomeController.cs
@@ -325,8 +325,10 @@ namespace LuduStack.Web.Controllers
                     "Set Game Characteristics"
                 }));
 
+            model.Items.Add(GenerateTimeLineItem(new DateTime(2020, 06, 01), "fas fa-rocket", "secondary", "June 2020", "New name!", "Now we are called LUDUSTACK! The one stop place for game developers."));
+
             // Future
-            model.Items.Add(GenerateTimeLineItem(new DateTime(2019, 11, 01), "fas fa-bug", "danger", "November 2020", "Open Beta", "At this point, we hope to have a consistent beta tester base so we can polish the platform and fix every possible bug tha shows up."));
+            model.Items.Add(GenerateTimeLineItem(new DateTime(2020, 11, 01), "fas fa-bug", "danger", "November 2020", "Open Beta", "At this point, we hope to have a consistent beta tester base so we can polish the platform and fix every possible bug tha shows up."));
 
             model.Items.Add(GenerateTimeLineItem(new DateTime(2021, 01, 01), "fas fa-star", "success", "January 2021", "Launch day!", "This is the scheduled launch day. On this day, all the core features will be implented."));
 
--- LuduStack.Web/Helpers/ConstantHelper.cs
+++ LuduStack.Web/Helpers/ConstantHelper.cs
@@ -4,9 +4,9 @@ namespace LuduStack.Web
 {
     public static class ConstantHelper
     {
-        public static string DefaultTitle { get { return "LUDUSTACK - The indie gamedev social network"; } }
+        public static string DefaultTitle { get { return "LUDUSTACK - The one stop place for game developers"; } }
 
-        public static string DefaultDescription { get { return "The independent game developers social network. Nothing can divide us!"; } }
+        public static string DefaultDescription { get { return "The one stop place for game developers, artists and musicians. Helping making gamedev dreams come true."; } }
 
         public static string DefaultAvatar { get { return Constants.DefaultAvatar; } }
     }
