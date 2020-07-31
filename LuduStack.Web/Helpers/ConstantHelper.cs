89ee7f48ecb60d5bee3483b5cf0098751dab159e
Daniel Gomes <programad@gmail.com>
2020-07-31 15:20:01 -0300

    fix(seo): long titles reduced

--- LuduStack.Web/Helpers/ConstantHelper.cs
+++ LuduStack.Web/Helpers/ConstantHelper.cs
@@ -7,6 +7,8 @@ namespace LuduStack.Web
 {
     public static class ConstantHelper
     {
+        public static string SiteName { get { return "LUDUSTACK"; } }
+
         public static string DefaultTitle { get { return "LUDUSTACK - The one stop place for game developers"; } }
 
         public static string DefaultDescription { get { return "The one stop place for game developers, artists and musicians. Helping making gamedev dreams come true."; } }
