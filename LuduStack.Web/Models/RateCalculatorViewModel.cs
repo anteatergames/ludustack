5785e7c9c20959051c819b45a4f48ba880f69389
Daniel Gomes <programad@gmail.com>
2020-09-21 10:02:47 -0300

    fix(rate calculator): translated to brazilian portuguese

--- LuduStack.Web/Models/RateCalculatorViewModel.cs
+++ LuduStack.Web/Models/RateCalculatorViewModel.cs
@@ -19,7 +19,7 @@ namespace LuduStack.Web.Models
         public double MontlyExpenses { get; set; }
 
         [Required]
-        [Display(Name = "Annual Cost", Description = "The amount of money needed to survive each year only paying the bills and buying food.")]
+        [Display(Name = "Annual Cost", Description = "Montly Expenses x 12")]
         [Range(1, double.MaxValue, ErrorMessage = "Need a number here!")]
         public double AnnualCost { get; set; }
 
@@ -28,10 +28,10 @@ namespace LuduStack.Web.Models
         [Range(1, double.MaxValue, ErrorMessage = "Need a number here!")]
         public double SavingsPercentage { get; set; }
 
-        [Display(Name = "per month", Description = "The amount of money needed for unplanned expenses each month")]
+        [Display(Name = "per month", Description = "The amount of money needed to save each month")]
         public double SavingsValueMonthly { get; set; }
 
-        [Display(Name = "per year", Description = "The amount of money needed for unplanned expenses each year")]
+        [Display(Name = "per year", Description = "The amount of money needed to save each year")]
         public double SavingsValueYearly { get; set; }
 
         [Required]
@@ -46,14 +46,14 @@ namespace LuduStack.Web.Models
         public double YourExtraValueYearly { get; set; }
 
         [Required]
-        [Display(Name = "Taxes %", Description = "The percentage of money you must pay as taxes of your country/state/city. Choose a value between 25%-40% according to where you live.")]
+        [Display(Name = "Taxes %", Description = "The percentage of money you must pay as taxes to your country/state/city. Choose a value between 25%-40% according to where you live.")]
         [Range(0, double.MaxValue, ErrorMessage = "Need a number here!")]
         public double TaxesPercentage { get; set; }
 
-        [Display(Name = "per month", Description = "The amount of money for taxes per month.")]
+        [Display(Name = "per month", Description = "The amount of money for taxes each month.")]
         public double TaxesValueMontly { get; set; }
 
-        [Display(Name = "per year", Description = "The amount of money for taxes per year.")]
+        [Display(Name = "per year", Description = "The amount of money for taxes each year.")]
         public double TaxesValueYearly { get; set; }
 
         [Required]
@@ -62,7 +62,7 @@ namespace LuduStack.Web.Models
         public double RevisionFactor { get; set; }
 
         [Required]
-        [Display(Name = "Rush Factor", Description = "The percentage to add to a rushed job. If the client want you to deliver fast, they must understand the overrate. We recommend a number between 50%-100%.")]
+        [Display(Name = "Rush Factor", Description = "The percentage to add to a rushed job. If the client want a fast delivery, it must understand the overrate. We recommend a number between 50%-100%.")]
         [Range(0, double.MaxValue, ErrorMessage = "Need a number here!")]
         public double RushFactor { get; set; }
 
