4e9f47612c668d0800cee7919df3c89a6e33d988
Daniel Gomes <programad@gmail.com>
2021-02-08 22:30:16 -0300

    feat(solution): packages update

---
+++ LuduStack.Domain.Core/Enums/SoundStyle.cs
@@ -0,0 +1,16 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace LuduStack.Domain.Core.Enums
+{
+    public enum SoundStyle
+    {
+        [Display(Name = "Chiptune")]
+        Chiptune = 1,
+
+        [Display(Name = "SNES Era")]
+        SnesEra = 2,
+
+        [Display(Name = "Orchestral")]
+        Orchestral = 3
+    }
+}
