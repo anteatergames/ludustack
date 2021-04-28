195108058d0b5c536b40fa6cfb630429650be7e7
Daniel Gomes <programad@gmail.com>
2021-04-28 13:04:25 -0300

    fix(dev): fixing dev env for macOS

--- LuduStack.Domain.Core/Attributes/ExternalLinkInfoAttribute.cs
+++ LuduStack.Domain.Core/Attributes/ExternalLinkInfoAttribute.cs
@@ -6,7 +6,7 @@ namespace LuduStack.Domain.Core.Attributes
     [AttributeUsage(AttributeTargets.Field)]
     public class ExternalLinkInfoAttribute : UiInfoAttribute
     {
-        public ExternalLinkType Type { get; set; }
+        public new ExternalLinkType Type { get; set; }
         public bool IsStore { get; set; }
         public string ColorClass { get; set; }
     }
