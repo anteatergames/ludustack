a8f1f2f12aab8334db92552669a21108c11972a7
Daniel Gomes <programad@gmail.com>
2021-09-06 10:39:48 -0300

    fix(game jam): no join button on voting phase

--- LuduStack.Domain.Messaging/Commands/Forum/SaveForumPostVoteCommand.cs
+++ LuduStack.Domain.Messaging/Commands/Forum/SaveForumPostVoteCommand.cs
@@ -3,7 +3,6 @@ using LuduStack.Domain.Interfaces;
 using LuduStack.Domain.Interfaces.Repository;
 using LuduStack.Domain.Interfaces.Services;
 using LuduStack.Domain.Messaging.Queries.ForumPost;
-using LuduStack.Domain.Messaging.Queries.Game;
 using LuduStack.Domain.Models;
 using LuduStack.Domain.ValueObjects;
 using LuduStack.Infra.CrossCutting.Messaging;
